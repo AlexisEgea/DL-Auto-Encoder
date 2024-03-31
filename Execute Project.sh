d590f0166c5b90c3c707d22958462d0ddfcedf9a
AlexisEgea <alexisegea@outlook.com>
2024-03-31 01:30:55 +0100

    Adding script .sh to execute the project with all the libraries needed

---
+++ Execute Project.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Auto-Encoder Execution"
+echo "_________________________________________________________________________________________________________________________________________________________"
+
+# shellcheck disable=SC2164
+
+echo "***Requirement: Have python3 & pip installed***"
+cd src/
+echo "Import library with Pip"
+echo
+
+pip install numpy
+pip install torch
+pip install matplotlib
+
+echo "_________________________________________________________________________________________________________________________________________________________"
+echo "Execution"
+
+python3 main.py
